c820538b4bcb3749d6bba56a9c38fc1e5bf72e7a
cjcj5438 <1002931123@qq.com>
2018-09-18 08:07:16 +0800

    1.2

--- listings/intro/first-project/index.js
+++ listings/intro/first-project/index.js
@@ -1,8 +1,10 @@
 var CountStream = require('./countstream'); //<co id="callout-intro-countstream-index-1" />
+// 正则是book的
 var countStream = new CountStream('book'); //<co id="callout-intro-countstream-index-2" />
 var https = require('https');
 
 https.get('https://www.manning.com', function(res) { //<co id="callout-intro-countstream-index-3" />
+  // pipe 通过打印发现,是dom 树  直接调用了  _write   私有方法.
   res.pipe(countStream); //<co id="callout-intro-countstream-index-4" />
 });
 
