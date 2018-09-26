8539d135e00f5dde33451903eb596c2b70dc3d43
chenjing <1002931123@qq.com>
2018-09-26 23:41:33 +0800

    update stream

--- listings/streams/node-0.10/objectmode.js
+++ listings/streams/node-0.10/objectmode.js
@@ -10,11 +10,14 @@ function MemoryStream(options) {
 }
 
 MemoryStream.prototype._read = function(size) {
+  // console.log(this)
   this.push(process.memoryUsage());// 用node 内置方法来创建一个对象 //<co id="listing-streams-objectmode-2" />
 };
 
 var memoryStream = new MemoryStream();
-memoryStream.on('readable', function() { //<co id="listing-streams-objectmode-3" />
+memoryStream.on('readable', function() { //添加一个监听器 <co id="listing-streams-objectmode-3" />
   var output = memoryStream.read();
   console.log('Type: %s, value: %j', typeof output, output);
 });
+
+// 为什么最后会输出三遍 console.log 应该是有一定的时效性把
