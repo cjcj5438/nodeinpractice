f9d9eba951a21fe7ed2ea4e05f695ff43b7e4ffb
cjcj5438 <1002931123@qq.com>
2018-09-27 22:44:13 +0800

    add 双工流

--- listings/streams/node-0.10/duplex.js
+++ listings/streams/node-0.10/duplex.js
@@ -1,18 +1,21 @@
 var stream = require('stream');
 
+//这和util的拷贝对象 是一样的 吗?
 HungryStream.prototype = Object.create(stream.Duplex.prototype, {
   constructor: { value: HungryStream }
 });
 
 function HungryStream(options) {
   stream.Duplex.call(this, options);
+  // 添加一个写的状态
   this.waiting = false; //<co id="callout-streams-duplex-1" />
 }
-
+/*enoding 不知道是做什么的
+* callback() 这样写除了代码可扩展, 有实际意义吗?*/
 HungryStream.prototype._write = function(chunk, encoding, callback) {
   this.waiting = false;
-  this.push('\u001b[32m' + chunk + '\u001b[39m');
-  callback(); //<co id="callout-streams-duplex-2" />
+  this.push('\u001b[32m' + chunk + '\u001b[39m'); // 把实现的数据推到内部队列,
+  callback(); // 然后执行内部的回掉函数
 };
 
 HungryStream.prototype._read = function(size) {
